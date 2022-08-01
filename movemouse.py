fbc344489787133412e8e690e8eafecd224fec6a
Czeropski <andrew.czeropski@bp.com>
2022-08-01 14:55:45 -0500

    Added shift button press on mouse move to ensure screen stays on for some machines

--- movemouse.py
+++ movemouse.py
@@ -3,19 +3,21 @@ import time
 
 
 def movemouse():
-    pyautogui.moveRel(10, -10, duration=0.25)
-    pyautogui.moveRel(-10, 10, duration=0.25)
+    pyautogui.moveRel(10, 0, duration=0.1)
+    pyautogui.moveRel(0, -10, duration=0.1)
+    pyautogui.moveRel(-10, 0, duration=0.1)
+    pyautogui.moveRel(0, 10, duration=0.1)
 
 
 if __name__ == '__main__':
-    i = True
     count = 0
     minutes = 5
-    while i:
+    while True:
         print('\r', minutes*60 - count, 'seconds left before moving mouse',
               end='', flush=True)
         if count >= minutes*60:
             movemouse()
+            pyautogui.press('shift')
             count = 0
         else:
             time.sleep(1)
